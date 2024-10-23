015eb9bcc2a8d83feeae8623f1c5134748ff1cb7
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2024-10-23 22:43:30 +0700

    console

--- ingestor/es_inserter/app.py
+++ ingestor/es_inserter/app.py
@@ -10,7 +10,6 @@ from typing import Dict, List
 
 app = FastAPI()
 logging.basicConfig(
-    filename='HISTORYlistener.log',
     level=logging.DEBUG,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
