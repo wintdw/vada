bcf3a40a69ffd93b65da89b271f32d7cc563c530
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2024-11-20 01:22:18 +0000

    do not work with bson

--- compose/etl/es_inserter/utils.py
+++ compose/etl/es_inserter/utils.py
@@ -12,21 +12,3 @@ def generate_docid(msg: Dict) -> str:
 
 def remove_fields(msg: Dict, fields_to_remove: List) -> Dict:
     return {k: v for k, v in msg.items() if k not in fields_to_remove}
-
-
-#### MONGO
-async def insert_mongo_mapping(
-    mongo_uri: str, db: str, collection: str, mapping_dict: dict
-):
-    client = AsyncIOMotorClient(mongo_uri)
-
-    try:
-        result = await client[db][collection].insert_one(mapping_dict)
-        logging.info(f"Mapping inserted successfully with _id: {result.inserted_id}")
-    except Exception as e:
-        logging.error(f"Error inserting mapping into MongoDB: {str(e)}")
-        logging.error("Traceback: ", exc_info=True)
-    finally:
-        client.close()
-
-    return result
