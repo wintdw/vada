ea8f62f2a2003b15b78de36a6193210b10cfbc3d
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2024-12-09 22:37:57 +0700

    index_mgmt

---
+++ compose/index_mgmt/app.py
@@ -0,0 +1,49 @@
+import os, sys
+from fastapi import FastAPI, HTTPException
+from typing import Dict
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from libs.async_es import AsyncESProcessor
+
+
+app = FastAPI()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+ELASTIC_URL = os.getenv("ELASTIC_URL", "")
+ELASTIC_USER = os.getenv("ELASTIC_USER", "")
+ELASTIC_PASSWD = ""
+# Passwd
+elastic_passwd_file = os.getenv("ELASTIC_PASSWD_FILE", "")
+if elastic_passwd_file and os.path.isfile(elastic_passwd_file):
+    with open(elastic_passwd_file, "r") as file:
+        ELASTIC_PASSWD = file.read().strip()
+
+es_processor = AsyncESProcessor(
+    es_baseurl=ELASTIC_URL, es_user=ELASTIC_USER, es_pass=ELASTIC_PASSWD
+)
+
+
+@app.get("/v1/index/{index_name}", response_model=Dict)
+async def get_index_info(index_name: str):
+    """
+    Get information about a specific Elasticsearch index if it exists.
+    """
+    try:
+        index_info = await es_processor.get_index(index_name)
+        if not index_info:
+            raise HTTPException(
+                status_code=404, detail=f"Index '{index_name}' not found."
+            )
+        return index_info
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    """Close the Elasticsearch session when the app shuts down."""
+    await es_processor.close()
---
+++ compose/index_mgmt/libs/security.py
@@ -0,0 +1,65 @@
+import jwt
+import logging
+from typing import Dict
+from pydantic import BaseModel
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+
+TOKEN_SECRET = ""
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+class JWTPayload(BaseModel):
+    name: str
+    id: str
+    exp: int
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "user7500",
+                "id": "65422b6aa2bbacc10b7a22a3",
+                "exp": 1731306868,
+            }
+        }
+
+
+def verify_jwt(token: str = Depends(oauth2_scheme)) -> Dict:
+    """
+    Function to verify the JWT Token header from client
+
+    Args:
+        token (str, optional): the JWT token provided. Defaults to Depends(oauth2_scheme).
+
+    Raises:
+        HTTPException: 401 upon Expired or Invalid tokens
+
+    Returns:
+        Dict: The decoded JWT info
+    """
+    try:
+        payload = jwt.decode(
+            token, TOKEN_SECRET, algorithms=["HS256"], options={"verify_exp": True}
+        )
+        logging.debug(f"Authenticated as {payload.get('name')}")
+
+        # Convert payload to JWTPayload model for validation
+        JWTPayload(**payload)
+
+        return payload
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="JWT token has expired"
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid JWT token"
+        )
