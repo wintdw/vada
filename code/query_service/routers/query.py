f4279a90fa288d4927d75bc829fb8d9199d82c93
Duc Nguyen Chi <ducnc.zz@gmail.com>
2024-12-26 15:09:28 +0000

    remove redirect

--- code/query_service/routers/query.py
+++ code/query_service/routers/query.py
@@ -7,7 +7,7 @@ from models import Payload
 router = APIRouter()
 
 #@router.get("/v1/query/", response_model=, tags=["Query"])
-@router.get("/v1/query/", tags=["Query"])
+@router.get("/v1/query", tags=["Query"])
 async def get_query(payload: Payload, jwt_dict: Dict = Depends(verify_jwt)):
     print(payload)
     return 200
