e54a400068270edf92de025f437dae71b95e1656
Duc Nguyen Chi <ducnc.zz@gmail.com>
2024-12-26 15:55:55 +0000

    forward to qe

--- code/query_service/routers/query.py
+++ code/query_service/routers/query.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends
 from typing import Dict
+from fastapi.responses import JSONResponse
 
 from tools import verify_jwt
 from models import Payload
 
 router = APIRouter()
 
-#@router.get("/v1/query/", response_model=, tags=["Query"])
-@router.get("/v1/query", tags=["Query"])
+@router.get("/v1/query", response_model=JSONResponse, tags=["Query"])
 async def get_query(payload: Payload, jwt_dict: Dict = Depends(verify_jwt)):
-    print(payload)
-    return 200
+    headers = {
+        "Content-Type": "application/json"
+    }
+    post_response = requests.post("https://dev-qe.vadata.vn/query", data=json.dumps(payload), headers=headers)
+    return JSONResponse(content=post_response.json())
