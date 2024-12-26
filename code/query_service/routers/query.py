a1becd7489c1dac47bf7711373163b725848054e
Duc Nguyen Chi <ducnc.zz@gmail.com>
2024-12-26 14:41:18 +0000

    add query_service

--- code/query_service/routers/query.py
+++ code/query_service/routers/query.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
+from typing import Dict
 
+from tools import verify_jwt
 from models import Payload
 
 router = APIRouter()
 
 #@router.get("/v1/query/", response_model=, tags=["Query"])
 @router.get("/v1/query/", tags=["Query"])
-async def get_query(payload: Payload):
+async def get_query(payload: Payload, jwt_dict: Dict = Depends(verify_jwt)):
     print(payload)
     return 200
