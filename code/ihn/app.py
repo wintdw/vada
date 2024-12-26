dcfc7833be9fa302453d85cb753c9533fac25e9d
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2024-12-26 21:37:45 +0700

    refine ihn receiver

--- code/ihn/app.py
+++ code/ihn/app.py
@@ -1,36 +1,62 @@
-from fastapi import FastAPI, Request, HTTPException # type: ignore
 import json
 import os
-from typing import Any, Dict
+import logging
+from datetime import datetime
+from fastapi import FastAPI, Request, HTTPException  # type: ignore
+from fastapi.responses import JSONResponse  # type: ignore
 
-# Create the FastAPI app instance
+
 app = FastAPI()
+logging.basicConfig(level=logging.DEBUG)
 
-# Path to the directory where we will write the request data
-DIR_PATH = '/app/data'
+
+DIR_PATH = "/app/data"
+
+
+@app.get("/health")
+async def check_health() -> JSONResponse:
+    """
+    Health check function
+
+    Returns:
+        JSONResponse: Return 200 when service is available
+    """
+    return JSONResponse(content={"status": "success", "detail": "Service Available"})
+
 
 @app.post("/capture")
-async def capture_json(request: Request) -> Dict[str, Any]:
-    # Parse the incoming JSON request body
-    json_data = await request.json()
+async def capture_json(request: Request):
+    """
+    Capture JSON data from the request and save it to a file.
 
-    # Ensure the directory exists
+    Args:
+        request (Request): The incoming request containing JSON data.
+
+    Returns:
+        JSONResponse: A response indicating the result of the operation.
+    """
+    try:
+        json_data = await request.json()
+    except json.JSONDecodeError:
+        logging.error(request.body())
+        raise HTTPException(status_code=400, detail="Invalid JSON data")
+
     os.makedirs(DIR_PATH, exist_ok=True)
 
-    # Extract the table_fullname from the JSON data
     table_fullname = json_data.get("table_fullname")
     if not table_fullname:
-        raise HTTPException(status_code=400, detail="Missing 'table_fullname' in the request data")
+        raise HTTPException(
+            status_code=400, detail="Missing 'table_fullname' in the request data"
+        )
 
-    # Construct the file path based on table_fullname
-    file_name = f"{table_fullname.replace('.', '_')}.json"
-    file_path = os.path.join(DIR_PATH, file_name)
+    date_str = datetime.now().strftime("%Y%m%d")
+    file_path = os.path.join(DIR_PATH, f"{table_fullname}_{date_str}.json")
 
-    # Write the received JSON data to the specified file
     try:
-        with open(file_path, 'a') as file:  # Open file in append mode
-            json.dump(json_data, file)  # Write as a JSON string
-            file.write("\n")  # Add a newline for each entry
-        return {"message": "Data captured successfully!"}
+        with open(file_path, "a", encoding="utf-8") as file:
+            json.dump(json_data, file)
+            file.write("\n")
+        return JSONResponse(content={"detail": "Data captured successfully!"})
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logging.error("Failed to write data to %s, %s", file_path, e)
+        raise HTTPException(status_code=500)
