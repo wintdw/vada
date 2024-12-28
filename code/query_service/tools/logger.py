ac50daec5f3ccd931f13bdf4e4dc88cf7ac33831
Duc Nguyen Chi <ducnc.zz@gmail.com>
2024-12-28 13:36:30 +0000

    logger add handler

--- code/query_service/tools/logger.py
+++ code/query_service/tools/logger.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 def setup_logger(name: str, level: int = logging.INFO):
 
@@ -8,6 +9,11 @@ def setup_logger(name: str, level: int = logging.INFO):
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
 
     return logger
 
