e88f91d77068ab295dbb3fbc26ef82d55a891c05
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-01-14 15:38:08 +0000

    ESexception

--- code/mappings/api/connectors/async_es.py
+++ code/mappings/api/connectors/async_es.py
@@ -3,6 +3,13 @@ from typing import Dict
 from aiohttp import ClientSession, ClientResponse, BasicAuth  # type: ignore
 
 
+class ESException(Exception):
+    def __init__(self, status_code: int, detail: str):
+        self.status_code = status_code
+        self.detail = detail
+        super().__init__(f"Status: {status_code} - Detail: {detail}")
+
+
 class AsyncESProcessor:
     def __init__(self, es_baseurl: str, es_user: str, es_pass: str):
         self.es_baseurl = es_baseurl
@@ -30,34 +37,6 @@ class AsyncESProcessor:
             logging.debug("Cluster health: %s", await response.text())
             return response
 
-    async def get_index(self, index_name: str) -> Dict:
-        """Check if an index exists and get its information if it does."""
-        es_url = f"{self.es_baseurl}/{index_name}"
-
-        await self._create_session()
-        async with self.session.head(es_url, auth=self.auth) as response:
-            if response.status == 200:
-                async with self.session.get(es_url, auth=self.auth) as response:
-                    if response.status == 200:
-                        index_info = await response.json()
-                        return index_info
-                    else:
-                        logging.error(
-                            "Failed to retrieve index info. Status: %s - %s",
-                            response.status,
-                            await response.text(),
-                        )
-            elif response.status == 404:
-                logging.error("Index %s does not exist.", index_name)
-            else:
-                logging.error(
-                    "Failed to check index info. Status: %s - %s",
-                    response.status,
-                    await response.text(),
-                )
-
-            return {}
-
     async def get_es_index_mapping(self, index_name: str) -> Dict:
         """Get the mapping of a specific Elasticsearch index."""
         es_url = f"{self.es_baseurl}/{index_name}/_mapping"
@@ -70,6 +49,7 @@ class AsyncESProcessor:
                     response.status,
                     await response.text(),
                 )
+                raise ESException(response.status, await response.text())
 
             mappings = await response.json()
             logging.info("Retrieved mappings for index: %s", index_name)
@@ -95,6 +75,7 @@ class AsyncESProcessor:
                     response.status,
                     await response.text(),
                 )
+                raise ESException(response.status, await response.text())
 
             return response
 
