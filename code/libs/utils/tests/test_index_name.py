d1a60a12c0ddd6bce382a18184ee273464dc7d3d
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-02-22 11:00:00 +0000

    correct

--- code/libs/utils/tests/test_index_name.py
+++ code/libs/utils/tests/test_index_name.py
@@ -2,12 +2,15 @@ from libs.utils.common import friendlify_index_name
 
 
 def test_friendlify_index_name():
-    self.assertEqual(friendlify_index_name("csv_dw_csv"), "CSV dw")
+    friendly_index_name = friendlify_index_name("csv_dw_csv")
+    assert friendly_index_name == "CSV dw"
 
 
 def test_friendlify_index_name_with_mul_underscores():
-    self.assertEqual(friendlify_index_name("csv_dw_dw_csv"), "CSV dw_dw")
+    friendly_index_name = friendlify_index_name("csv_dw_dw_csv")
+    assert friendly_index_name == "CSV dw_dw"
 
 
 def test_friendlify_index_name_empty_string():
-    self.assertEqual(friendlify_index_name(""), "")
+    friendly_index_name = friendlify_index_name("")
+    assert friendly_index_name == ""
