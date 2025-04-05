2e8082d24648e9f5cca5406bcfb320ec77556e0c
Duc Nguyen <ducnc@torus-actions.fr>
2025-04-05 19:36:18 +0700

    tiktok biz model

--- code/tiktok/crawler-api/models/tiktok.py
+++ code/tiktok/crawler-api/models/tiktok.py
@@ -4,11 +4,52 @@ class Advertiser(BaseModel):
   advertiser_id: str
   advertiser_name: str
 
-class AdvertiserList(BaseModel):
-  list: list[Advertiser]
+class AdvertiserInfo(BaseModel):
+  rejection_reason: str | None
+  license_no: str
+  timezone: str
+  brand: str | None
+  promotion_center_city: str | None
+  contacter: str
+  status: str
+  industry: str
+  currency: str
+  country: str
+  create_time: int
+  telephone_number: str
+  company: str
+  address: str | None
+  license_city: str | None
+  language: str
+  license_url: str | None
+  advertiser_account_type: str
+  license_province: str | None
+  balance: int
+  cellphone_number: str
+  promotion_center_province: str | None
+  role: str
+  advertiser_id: str
+  description: str
+  name: str
+  email: str
+  promotion_area: str
+  display_timezone: str
 
-class AdvertiserResponse(BaseModel):
+class PageInfo(BaseModel):
+    page: int
+    page_size: int
+    total_page: int
+    total_number: int
+
+class ReportIntegrated(BaseModel):
+  nothing: None = None 
+
+class DataPayload(BaseModel):
+  list: list[Advertiser | AdvertiserInfo | ReportIntegrated]
+  page_info: PageInfo = None
+
+class TiktokBusinessResponse(BaseModel):
   code: int
   message: str
   request_id: str
-  data: AdvertiserList
+  data: DataPayload | None
--- code/tiktok/crawler-api/routers/tiktok.py
+++ code/tiktok/crawler-api/routers/tiktok.py
@@ -2,23 +2,81 @@ from fastapi import APIRouter, HTTPException
 import json
 
 from tools import get_logger
-from models import AdvertiserResponse
+from models import TiktokBusinessResponse
 
 router = APIRouter()
 logger = get_logger(__name__, 20)
 
-@router.get("/v1/tiktok_biz/advertiser/get/", response_model=AdvertiserResponse, tags=["Tiktok"])
+@router.get("/v1/tiktok_business/get/", tags=["Tiktok"])
+async def tiktok_business_get(start_date: str, end_date: str, page: int, page_size: int):
+  from services import tiktok_biz_get_advertiser, tiktok_biz_info_advertiser, tiktok_biz_get_report_integrated
+
+  advertiser_response = TiktokBusinessResponse.model_validate(await tiktok_biz_get_advertiser())
+  logger.info(advertiser_response)
+
+  dimensions = ["ad_id", "stat_time_day"]
+  metrics = [
+      "spend",
+      "billed_cost",
+      "cash_spend",
+      "voucher_spend",
+      "cpc",
+      "cpm",
+      "impressions",
+      "gross_impressions",
+      "clicks",
+      "ctr",
+      "reach",
+      "cost_per_1000_reached",
+      "frequency",
+      "conversion",
+      "cost_per_conversion",
+      "conversion_rate",
+      "conversion_rate_v2",
+      "real_time_conversion",
+      "real_time_cost_per_conversion",
+      "real_time_conversion_rate",
+      "real_time_conversion_rate_v2",
+      "result",
+      "cost_per_result",
+      "result_rate",
+      "real_time_result",
+      "real_time_cost_per_result",
+      "real_time_result_rate",
+      "secondary_goal_result",
+      "cost_per_secondary_goal_result",
+      "secondary_goal_result_rate",
+  ]
+
+  for advertiser in advertiser_response.data.list:
+    advertiser_info_response = TiktokBusinessResponse.model_validate(await tiktok_biz_info_advertiser(params={"advertiser_ids": json.dumps([advertiser.advertiser_id])}))
+    logger.info(advertiser_info_response)
+
+    report_integrated_response = TiktokBusinessResponse.model_validate(await tiktok_biz_get_report_integrated(params={
+      "advertiser_id": advertiser.advertiser_id,
+      "start_date": start_date,
+      "end_date": end_date,
+      "report_type": "BASIC",
+      "data_level": "AUCTION_AD",
+      "dimensions": json.dumps(dimensions),
+      "metrics": json.dumps(metrics),
+      "page": page,
+      "page_size": page_size
+    }))
+    logger.info(report_integrated_response)
+
+@router.get("/v1/tiktok_biz/advertiser/get/", response_model=TiktokBusinessResponse, tags=["Tiktok"])
 async def tiktok_biz_advertiser_get():
   from services import tiktok_biz_get_advertiser
 
-  advertiser_response = AdvertiserResponse.model_validate(await tiktok_biz_get_advertiser())
+  advertiser_response = TiktokBusinessResponse.model_validate(await tiktok_biz_get_advertiser())
   return advertiser_response
 
 @router.get("/v1/tiktok_biz/advertiser/info/", tags=["Tiktok"])
 async def tiktok_biz_advertiser_info():
   from services import tiktok_biz_get_advertiser, tiktok_biz_info_advertiser
 
-  advertiser_response = AdvertiserResponse.model_validate(await tiktok_biz_get_advertiser())
+  advertiser_response = TiktokBusinessResponse.model_validate(await tiktok_biz_get_advertiser())
   for advertiser in advertiser_response.data.list:
     return await tiktok_biz_info_advertiser(params={"advertiser_ids": json.dumps([advertiser.advertiser_id])})
 
@@ -26,7 +84,7 @@ async def tiktok_biz_advertiser_info():
 async def tiktok_biz_campaign_get(page: int, page_size: int):
   from services import tiktok_biz_get_advertiser, tiktok_biz_get_campaign
 
-  advertiser_response = AdvertiserResponse.model_validate(await tiktok_biz_get_advertiser())
+  advertiser_response = TiktokBusinessResponse.model_validate(await tiktok_biz_get_advertiser())
   for advertiser in advertiser_response.data.list:
     return await tiktok_biz_get_campaign(params={"advertiser_id": advertiser.advertiser_id, "page": page, "page_size": page_size})
 
@@ -34,7 +92,7 @@ async def tiktok_biz_campaign_get(page: int, page_size: int):
 async def tiktok_biz_report_integrated_get(start_date: str, end_date: str, page: int, page_size: int):
   from services import tiktok_biz_get_advertiser, tiktok_biz_get_report_integrated
 
-  advertiser_response = AdvertiserResponse.model_validate(await tiktok_biz_get_advertiser())
+  advertiser_response = TiktokBusinessResponse.model_validate(await tiktok_biz_get_advertiser())
 
   dimensions = ["ad_id", "stat_time_day"]
   metrics = [
