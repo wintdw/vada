8650bd724d8ed67e19de4546da36066d1ce47fb5
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-04-10 15:23:14 +0000

    init doc_id

--- code/libs/connectors/async_es.py
+++ code/libs/connectors/async_es.py
@@ -149,6 +149,7 @@ class AsyncESProcessor:
         # Prepare the bulk request payload
         bulk_payload = ""
         for doc in docs:
+            doc_id = None
             # Find the doc_id in the metadata, if appears
             if "_vada" in doc:
                 doc_id = doc.get("_vada", {}).get("ingest", {}).get("doc_id", "")
