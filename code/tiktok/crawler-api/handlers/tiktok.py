65813f4df486375131841bd38a140bbde6f4f806
Duc Nguyen <ducnc@torus-actions.fr>
2025-04-13 20:32:55 +0700

    format report

--- code/tiktok/crawler-api/handlers/tiktok.py
+++ code/tiktok/crawler-api/handlers/tiktok.py
@@ -8,7 +8,8 @@ def create_report(
     campaign: dict,
     adgroup: dict,
 ) -> dict:
-    return advertiser_info.get("data").get("list")[0] | report_integrated.get("data").get("list")[0] | ad.get("data").get("list")[0] | campaign.get("data").get("list")[0] | adgroup.get("data").get("list")[0]
+    report_integrated_flattened = dict(item for d in report_integrated.get("data").get("list")[0].values() for item in d.items())
+    return report_integrated_flattened | {"advertiser": advertiser_info.get("data").get("list")[0]} | {"ad": ad.get("data").get("list")[0]} | {"campaign": campaign.get("data").get("list")[0]} | {"adgroup": adgroup.get("data").get("list")[0]}
 
 def save_report(data, filename):
   with open(filename, 'a', encoding='utf-8') as f:
@@ -26,7 +27,7 @@ def enrich_report(report: dict, doc_id: str) -> dict:
           "source": "crawling:tiktok_ad_detailed",
           "destination": {
             "type": "elasticsearch",
-            "index": "a_quang_nguyen_tiktok_ad_detailed"
+            "index": "a_quang_nguyen_tiktok_ad_report"
           },
           "vada_client_id": "a_quang_nguyen",
           "type": "tiktok_ad_detailed",
