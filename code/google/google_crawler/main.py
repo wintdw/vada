32333ed9f48e5afe5a97347157485aa1788c6cec
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-04-20 17:40:57 +0000

    feat: include crawl router in FastAPI application

--- code/google/google_crawler/main.py
+++ code/google/google_crawler/main.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from fastapi import FastAPI  # type: ignore
-from router import auth
+from router import auth, crawl
 
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
 
@@ -14,8 +14,8 @@ app = FastAPI(title="Google OAuth API")
 # Store the flow object in a global variable or use a more robust solution like a database in production
 flows = {}
 
-# Include the auth router
 app.include_router(auth.router, tags=["auth"])
+app.include_router(crawl.router, tags=["auth"])
 
 # Pass the flows dictionary to router endpoints that need it
 app.state.flows = flows
