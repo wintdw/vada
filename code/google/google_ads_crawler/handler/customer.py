9888f1e229f4eb8bc66995cd14e0e296b9451644
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-04-21 18:19:51 +0000

    refactor: enhance child account retrieval by filtering based on status

--- code/google/google_ads_crawler/handler/customer.py
+++ code/google/google_ads_crawler/handler/customer.py
@@ -174,10 +174,15 @@ async def get_child_accounts(ga_client: GoogleAdsClient, manager_id: str) -> Lis
                 customer_client.applied_labels,
                 customer_client.client_customer,
                 customer_client.level,
-                customer_client.manager
+                customer_client.manager,
+                customer_client_link.status
             FROM customer_client
             WHERE customer_client.status = 'ENABLED'
-        """
+            AND customer_client.id != {manager_id}
+            AND customer_client_link.status = 'ACTIVE'
+        """.format(
+            manager_id=manager_id
+        )
 
         ga_service = ga_client.get_service("GoogleAdsService")
         response = ga_service.search(
