29a067a4694e252234960467a97db7a24d801dbc
Duc Nguyen <ducnc@gmail.com>
2025-04-27 21:50:39 +0700

    fix prom

--- code/crawler/tiktok/tiktok_ads_crawler/routers/schedule.py
+++ code/crawler/tiktok/tiktok_ads_crawler/routers/schedule.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from datetime import timedelta
+from prometheus_client import Gauge
 
 from tools import get_logger
 from models import CrawlHistory, CrawlInfo, CrawlInfoResponse
@@ -7,18 +8,15 @@ from models import CrawlHistory, CrawlInfo, CrawlInfoResponse
 router = APIRouter()
 logger = get_logger(__name__, 20)
 
+active_crawl_jobs_gauge = Gauge("active_crawl_jobs", "Number of active jobs")
+
 @router.get("/v1/schedule")
 async def update_metrics():
     from repositories import select_crawl_history_by_crawl_status
-    from prometheus_client import Gauge
 
     try:
         crawl_history = await select_crawl_history_by_crawl_status()
-        crawl_gauge = Gauge(
-            "active_crawl_jobs",
-            "Number of active jobs"
-        )
-        crawl_gauge.set(len(crawl_history))
+        active_crawl_jobs_gauge.set(len(crawl_history))
         logger.info(f"Metrics updated")
     except Exception as e:
         logger.error(f"Error updating metrics: {e}")
