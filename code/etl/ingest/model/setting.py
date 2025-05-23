1f38a43c93aca203fa3a30bdd909a20eb7e3c620
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-05-23 04:14:04 +0000

    correct env

--- code/etl/ingest/model/setting.py
+++ code/etl/ingest/model/setting.py
@@ -5,9 +5,6 @@ class Settings(BaseSettings):
     # Required settings (must be set in env or .env)
     PORT: str
     APP_ENV: str
-    ELASTIC_PASSWD_FILE: str
-    ELASTIC_USER: str
-    ELASTIC_URL: str
     MAPPINGS_BASEURL: str
     INSERT_BASEURL: str
 
