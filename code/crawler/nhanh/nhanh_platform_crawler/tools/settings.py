d9985abb97a4bbf34e1e0777981534e0e13af6eb
Duc Nguyen Chi <ducnc.zz@gmail.com>
2025-07-04 08:59:19 +0000

    add env nhanh

--- code/crawler/nhanh/nhanh_platform_crawler/tools/settings.py
+++ code/crawler/nhanh/nhanh_platform_crawler/tools/settings.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 import os
 
 class Settings(BaseSettings):
+    NHANH_REDIRECT_URI: str = os.getenv("NHANH_REDIRECT_URI")
     INSERT_SERVICE_URL: str = os.getenv("INSERT_SERVICE_URL")
     CONNECTOR_CALLBACK_URL: str = os.getenv("CONNECTOR_CALLBACK_URL")
 
