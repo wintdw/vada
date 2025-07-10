70c1fcadefa73e87e238eba8bc45e4fa7c8d2752
Duc Nguyen <ducnc@torus-actions.fr>
2025-07-10 21:28:34 +0700

    fix acl

--- code/acl/acl-api/sql/schema.sql
+++ code/acl/acl-api/sql/schema.sql
@@ -36,6 +36,16 @@ CREATE TABLE `GroupSetting` (
 
 CREATE TABLE `UserSetting` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `setting` json NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+CREATE TABLE `UserSetting_v2` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `workspace_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `setting` json NOT NULL,
