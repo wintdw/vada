6726d1500725d52df6daf902d719a4717724b70f
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-07-26 08:26:09 +0000

    logging on tta scheduler

--- code/crawler/tiktok/tiktok_ads_crawler/schedulers/main.py
+++ code/crawler/tiktok/tiktok_ads_crawler/schedulers/main.py
@@ -69,72 +69,75 @@ async def init_scheduler():
     scheduler = AsyncIOScheduler()
 
     async def update_jobs():
-        # Fetch crawl info specific to TikTok Shop
-        crawl_infos = await get_crawl_info()
-        tasks = []
-        current_jobs = {job.id: job for job in scheduler.get_jobs()}
+        try:
+            # Fetch crawl info specific to TikTok Shop
+            crawl_infos = await get_crawl_info()
+            tasks = []
+            current_jobs = {job.id: job for job in scheduler.get_jobs()}
 
-        for info in crawl_infos:
-            crawl_id = info["crawl_id"]
-            account_name = info["account_name"]
-            index_name = info["index_name"]
-            access_token = info["access_token"]
-            crawl_interval = info["crawl_interval"]
-            last_crawl_time = info["last_crawl_time"]
-            first_crawl = False
-            if not last_crawl_time:
-                first_crawl = True
+            for info in crawl_infos:
+                crawl_id = info["crawl_id"]
+                account_name = info["account_name"]
+                index_name = info["index_name"]
+                access_token = info["access_token"]
+                crawl_interval = info["crawl_interval"]
+                last_crawl_time = info["last_crawl_time"]
+                first_crawl = False
+                if not last_crawl_time:
+                    first_crawl = True
 
-            job_id = f"crawl_tta_{crawl_id}"
+                job_id = f"crawl_tta_{crawl_id}"
 
-            job = scheduler.get_job(job_id)
-            should_update = False
-            # job exists
-            if job:
-                job_access_token = job.kwargs.get("access_token")
-                job_crawl_interval = (
-                    job.trigger.interval.total_seconds() // 60
-                    if hasattr(job.trigger, "interval")
-                    else None
-                )
-                # Only update if refresh_token or crawl_interval changed
-                if (
-                    job_access_token != access_token
-                    or job_crawl_interval != crawl_interval
-                ):
-                    should_update = True
-            # new job
-            else:
-                should_update = True
-
-            if should_update:
-                tasks.append(
-                    add_tiktok_ad_crawl_job(
-                        scheduler=scheduler,
-                        job_id=job_id,
-                        crawl_id=crawl_id,
-                        access_token=access_token,
-                        index_name=index_name,
-                        crawl_interval=crawl_interval,
-                        account_name=account_name,
-                        first_crawl=first_crawl,
+                job = scheduler.get_job(job_id)
+                should_update = False
+                # job exists
+                if job:
+                    job_access_token = job.kwargs.get("access_token")
+                    job_crawl_interval = (
+                        job.trigger.interval.total_seconds() // 60
+                        if hasattr(job.trigger, "interval")
+                        else None
                     )
-                )
+                    # Only update if refresh_token or crawl_interval changed
+                    if (
+                        job_access_token != access_token
+                        or job_crawl_interval != crawl_interval
+                    ):
+                        should_update = True
+                # new job
+                else:
+                    should_update = True
 
-            if job_id in current_jobs:
-                del current_jobs[job_id]
+                if should_update:
+                    tasks.append(
+                        add_tiktok_ad_crawl_job(
+                            scheduler=scheduler,
+                            job_id=job_id,
+                            crawl_id=crawl_id,
+                            access_token=access_token,
+                            index_name=index_name,
+                            crawl_interval=crawl_interval,
+                            account_name=account_name,
+                            first_crawl=first_crawl,
+                        )
+                    )
 
-        # Remove jobs that are no longer in the crawl_info
-        for left_job_id in current_jobs:
-            if left_job_id != "update_jobs":
-                scheduler.remove_job(left_job_id)
-                logger.info(
-                    f"[Scheduler] Removed TikTokAd job_id '{left_job_id}' as it is no longer valid"
-                )
+                if job_id in current_jobs:
+                    del current_jobs[job_id]
 
-        # Wait for all add_google_ad_crawl_job tasks to finish
-        if tasks:
-            await asyncio.gather(*tasks)
+            # Remove jobs that are no longer in the crawl_info
+            for left_job_id in current_jobs:
+                if left_job_id != "update_jobs":
+                    scheduler.remove_job(left_job_id)
+                    logger.info(
+                        f"[Scheduler] Removed TikTokAd job_id '{left_job_id}' as it is no longer valid"
+                    )
+
+            # Wait for all add_google_ad_crawl_job tasks to finish
+            if tasks:
+                await asyncio.gather(*tasks)
+        except Exception as e:
+            logger.error(f"[Scheduler] Error TikTokAd jobs: {str(e)}", exc_info=True)
 
     # Schedule the update_jobs function to run every 1m
     scheduler.add_job(
