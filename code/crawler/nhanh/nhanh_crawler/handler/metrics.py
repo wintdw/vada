700d53744bfb740d9f8a12cfa0dd12da9693f338
Dung Trung Nguyen <ifonlyitweretruedw@gmail.com>
2025-08-05 16:20:51 +0000

    app env

--- code/crawler/nhanh/nhanh_crawler/handler/metrics.py
+++ code/crawler/nhanh/nhanh_crawler/handler/metrics.py
@@ -2,16 +2,17 @@ from datetime import datetime
 from prometheus_client import Gauge  # type: ignore
 from fastapi import APIRouter  # type: ignore
 
+from model.settings import settings
 from .crawl_info import get_crawl_info
 
 router = APIRouter()
 
 # Define Prometheus Gauges
 last_crawl_time_gauge = Gauge(
-    "nhanh_last_crawl_time", "Last crawl time (epoch seconds)", ["crawl_id"]
+    "nhanh_last_crawl_time", "Last crawl time (epoch seconds)", ["crawl_id", "app_env"]
 )
 next_crawl_time_gauge = Gauge(
-    "nhanh_next_crawl_time", "Next crawl time (epoch seconds)", ["crawl_id"]
+    "nhanh_next_crawl_time", "Next crawl time (epoch seconds)", ["crawl_id", "app_env"]
 )
 
 
@@ -25,12 +26,16 @@ async def update_crawl_metrics():
         if last_crawl_time:
             try:
                 dt = datetime.fromisoformat(last_crawl_time)
-                last_crawl_time_gauge.labels(crawl_id=crawl_id).set(dt.timestamp())
+                last_crawl_time_gauge.labels(
+                    crawl_id=crawl_id, app_env=settings.APP_ENV
+                ).set(dt.timestamp())
             except Exception:
                 pass
         if next_crawl_time:
             try:
                 dt = datetime.fromisoformat(next_crawl_time)
-                next_crawl_time_gauge.labels(crawl_id=crawl_id).set(dt.timestamp())
+                next_crawl_time_gauge.labels(
+                    crawl_id=crawl_id, app_env=settings.APP_ENV
+                ).set(dt.timestamp())
             except Exception:
                 pass
